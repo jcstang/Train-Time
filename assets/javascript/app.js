e0db9d2e7e48a149a651a6618ca752252e931fba
Jacob Stanger <jacobcstanger@gmail.com>
2020-02-09 11:58:17 -0700

    remove old comment code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,45 +22,9 @@ $(document).ready(function () {
   // has a child been added to DB?
   // ===================================================
   database.ref().on('child_added', function(child_snapshot) {
-      // var employeeData = child_snapshot.val();
+
       var trainData = child_snapshot.val();
-
       const row = $('<tr>');
-
-      // OLD DATA
-      // const name = $('<td>').text(employeeData.name);
-      // const role = $('<td>').text(employeeData.role);
-      // var dateString = moment.unix(employeeData.startDate).format('MM/DD/YYYY'); // input: 1273816800, output: 05/14/2010
-      // const startDate = $('<td>').text( dateString );
-
-      
-      
-      // ===== diff in months,  from start date to now ==============
-      // var timeNow = moment(Date.now());
-      // var diffMonths = timeNow.diff( moment(dateString), 'months', true );
-      // var howManyMonthsWorked = Math.floor(diffMonths);
-      
-      // =============== calc totalBilled ===========================
-      // var howMuchEarned = employeeData.monthlyRate * howManyMonthsWorked;
-      // console.log('howMuchEarned: ', howMuchEarned);
-      // howMuchEarned = howMuchEarned.toFixed(2);
-      // console.log('howMuchEarned: ', howMuchEarned);
-      
-      
-      // const monthsWorked = $('<td>').text('');
-      // const monthsWorked = $('<td>').text(howManyMonthsWorked);
-      // const monthlyRate = $('<td>').text(employeeData.monthlyRate);
-      // const totalBilled = $('<td>').text('$ ' + numberWithCommas(howMuchEarned) );
-      
-      // row
-      //     .append(name)
-      //     .append(role)
-      //     .append(startDate)
-      //     .append(monthsWorked)
-      //     .append(monthlyRate)
-      //     .append(totalBilled);
-      
-      // $('tbody').append(row);
       
 
       const trainName = $('<td>').text(trainData.name);
@@ -87,25 +51,16 @@ $(document).ready(function () {
   $('button[type="submit"]').on('click', function (event) {
       event.preventDefault();
 
-      // var name = $('#employee-name').val();
       var trainName = $('#train-input').val();
-      // console.log(trainName);
-      
-      // var role = $('#role').val();
+
       var trainDestination = $('#dest-input').val().trim();
-      // var startDate = $('#start-date').val();
       var trainFirstTime = $('#first-train-input').val().trim();
-      // var monthlyRate = $('#monthly-rate').val();
       var trainFrequency = $('#freq-input').val().trim();
 
       console.log(trainName, trainDestination, trainFirstTime, trainFrequency);
       
 
       database.ref().push({
-          // name: name,
-          // role: role,
-          // startDate: moment(startDate, 'MM/DD/YYYY').format('X'),
-          // monthlyRate: monthlyRate
           name: trainName,
           destination: trainDestination,
           firstTrainTime: trainFirstTime,
